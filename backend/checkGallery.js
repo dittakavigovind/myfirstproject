97c740a956e2a76e55de97bc5572812ca2d39b9a
dittakavigovind <dgovind78@gmail.com>
2026-06-03 00:25:56 +0530

    Ignore scratch scripts and cleanup repository

--- backend/checkGallery.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-    const Astrologer = require('./src/models/Astrologer');
-    const astros = await Astrologer.find({ gallery: { $exists: true, $not: {$size: 0} } }).select('displayName gallery');
-    console.log(JSON.stringify(astros, null, 2));
-    process.exit(0);
-}).catch(console.error);
--- backend/update_wallet2.js
+++
@@ -1,6 +0,0 @@
-require('mongoose').connect(require('dotenv').config().parsed.MONGO_URI).then(async () => {
-    const User = require('./src/models/User');
-    await User.updateOne({ phone: '+919849097924' }, { $set: { walletBalance: 1000 } });
-    console.log('Wallet updated!');
-    process.exit(0);
-}).catch(console.error);
